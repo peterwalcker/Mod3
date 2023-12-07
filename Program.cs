366f8d2a00c9a7e38452f20416a92b926e0d2aa8
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2023-12-07 22:47:50 +0300

    done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double result = 10 % 3;
+            Console.WriteLine(result);
         }
     }
 }
