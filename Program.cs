8b2f666b5aa8449ca8e99a3990ce0e53d08590e2
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2023-12-07 23:13:13 +0300

    Questionnaire

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your birthdate: ");
+            var birthdate = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {name} and age is {age}.\nYour birthdate is {birthdate}");
+            Console.ReadKey();
         }
     }
 }
