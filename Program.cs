e7974258b4c468d0e05e1f77f76cd2d8731bddfc
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2023-12-07 21:54:43 +0300

    Using different variable types Mod_3_3_2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Mod3
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Mod3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string Name = "Johan";
+            byte Age = 40;
+            bool IsPetOwner = true;
+            double ShoeSize = 46.5;
+
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My Age is " + Age);
+            Console.WriteLine("Do I have a pet? " + IsPetOwner);
+            Console.WriteLine("My shoe size is " + ShoeSize);
         }
     }
 }
